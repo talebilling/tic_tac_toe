c75a825da71bb69fa8b041dfb7b11f8113b36616
Emese Billing <talebilling@gmail.com>
2016-10-12 18:17:14 +0200

    refact change variables

--- tic_tac_toe_initial.py
+++ tic_tac_toe_initial.py
@@ -5,66 +5,75 @@ def print_board(board):
     for i in range(3):
         print(" ", end="")
         for j in range(3):
-            if 0 < move:
+            #print X or O, after user gave input
+            if 0 < move_count:
                 if board[i*3+j] == 1:
                     print('X', end="")
                 elif board[i*3+j] == 0:
                     print('O', end="")
                 else: 
                     print(' ', end="")
-            elif board[i*3+j] != -1:
-                print(board[i*3+j], end="") #print board with instruction numbers
+            #print board with instruction numbers
+            elif board[i*3+j] != 'filled in':
+                print(board[i*3+j], end="") 
             else:
                 print(' ', end="")
-            
+            #print sepatation wall
             if j != 2:
                 print(" | ", end="")
-        
+        #print sepatation wall
         if i != 2:
             print("\n""-----------")
     print("\n")
 
 def print_instruction():
-	print("You can use the folling numbers to make your move\n")
-    #print("The board look like this: )
+	print("You can use the folling numbers to make your move:\n")
 	print_board([1,2,3,4,5,6,7,8,9])
 
 def user_input(player):
-    valid = False
-    while not valid: 
+    while True: 
         try: 
-            print()
-            user = input("Turn - Player " + player + " - Where would you " +
+            player_input = input("Turn: Player " + player + " - Where would you " +
                         "like to place the " + player + " mark? ")
             print('\n')
-            user = int(user)
-            if user >=1 and user <= 9: 
-                return user - 1
+            player_input = int(player_input)
+            if player_input >=1 and player_input <= 9: 
+                return player_input - 1
             else: 
                 print("That is not a valid move, please try again.\n")
         except Exception as e:
             print("This is not a valid move, please try again.\n")
 
+def check_win():
+    print
+
+def quit_game():
+    print
+
 #main
 clear = os.system('cls' if os.name == 'nt' else 'clear')
-move = 0
-player = 'X'
 board = [' '] * 9
+move_count = 0
+player = 'X'
 win = False
 
 print_instruction()
 
 while not win:
-    move += 1
-    user = user_input(player)
+    move_count += 1
+    player_input = user_input(player)
     
-    if move % 2 == 0:
+    if move_count % 2 == 0:
         player = 'X'
     else: 
         player = 'O'
     
     if player == 'O':
-        board[user] = 1
+        board[player_input] = 1
     else:
-        board[user] = 0
+        board[player_input] = 0
     print_board(board)
+
+
+check_win()
+quit_game()
